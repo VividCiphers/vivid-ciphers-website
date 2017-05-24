7597204423dbbfa58b3b575e45d39950b1d57a36
gp22 <gp22@tutanota.com>
2017-05-24 13:05:37 +0700

    add projects service

--- src/app/shared/projects.service.ts
+++ src/app/shared/projects.service.ts
@@ -11,7 +11,7 @@ export class ProjectsService {
   // Dates for fake project data.
   created: Date = new Date();
   updated: Date = new Date();
-  
+
   projects: Project[] = [
     new Project({ id: '123', name: 'Vivid Ciphers Rework', description: 'Rebuild website', created: this.created, updated: this.updated, active: true }),
     new Project({ id: '456', name: 'Social Media Project', description: 'Build social media app', created: this.created, updated: this.updated, active: true }),
@@ -24,33 +24,62 @@ export class ProjectsService {
   getAllProjects() {
     return this.projects;
   }
-  
-  getProject(id) {
+
+  getProjectById(id: string) {
     /*
     Takes one argument, id: string
     Returns the project with id or undefined if it doesn't exist.
     */
-    function findProjectById(project) {
-      return project.id === id;
-    }
-    return this.projects.find(findProjectById);
+    return this.projects[this.getProjectIndexById(id)];
   }
-  
-  createProject(project) {
+
+  createProject(project: {}) {
     /*
-    Takes one argument, project: Project
+    Takes one argument, project: Object;
     Creates the project, adds it to projects, and returns the created project.
     */
     const newProject = new Project(project);
     this.projects.push(newProject);
     return newProject;
   }
-  
-  deleteProject() {
 
+  deleteProjectById(id: string) {
+    /*
+    Takes one argument, id: string
+    Removes project with id and returns it, or undefined if it doesn't exist.
+    */
+    const index: number = this.getProjectIndexById(id);
+    if (index !== -1) {
+      const deletedProject: Project = this.projects[index];
+      this.projects.splice(index, 1);
+      return deletedProject;
+    }
+    return undefined;
   }
-  
-  updateProject() {
 
+  updateProjectById(id: string, propertyToUpdate: {}) {
+    /*
+    Takes two arguments, id: string, and propertyToUpdate: an object with the
+    key, value pair of the property to update. ex. { name: 'new name' }
+    Updates project with id and returns it, or undefined if it doesn't exist.
+    */
+    const index: number = this.getProjectIndexById(id);
+    if (index === -1) return undefined;
+    const keys = (Object.keys(propertyToUpdate));
+    keys.forEach((key) => {
+      this.projects[index][key] = propertyToUpdate[key];
+    });
+    return this.projects[index];
+  }
+
+  private getProjectIndexById = function(id: string) {
+    /*
+    Internal helper function for finding project indexes by their id. Not to
+    be used outside of projects.service.
+    */
+    function findProjectById(project: Project) {
+      return project.id === id;
+    }
+    return this.projects.findIndex(findProjectById);
   }
 }
